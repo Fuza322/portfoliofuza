e34b2f48871b64d35f690d589bd9cc563aee1586
Fuza322 <vanya.osipik322@gmail.com>
2021-03-06 20:51:09 +0300

    test commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ function App() {
     return (
         <div className="App">
             Try to deploy github pages
+        <div>
+            TEST TEST TEST
+        </div>
         </div>
     );
 }
