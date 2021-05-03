067d35443380a33b75d724727ba454f7fc9a9401
Fuza322 <vanya.osipik322@gmail.com>
2021-05-03 21:20:43 +0300

    fix 1700px media-queries

--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Particles from "react-particles-js";
 import ReactTypingEffect from 'react-typing-effect';
 import style from './Main.module.scss'
 
@@ -6,32 +7,73 @@ export type MainType = {
     style: { backgroundImage: string }
 }
 
+const particlesOptions = {
+    "particles": {
+        "number": {"value": 90, "density": {"enable": true, "value_area": 800}},
+        "color": {"value": "#FF9000"},
+        "shape": {
+            "type": "circle",
+            "stroke": {"width": 0, "color": "#"},
+            "polygon": {"nb_sides": 5},
+        },
+        "size": {"value": 7, "random": true, "anim": {"enable": false, "speed": 40, "sync": false}},
+    }
+}
+
 export function Main(props: MainType) {
     return (
         <section className={style.mainBlock}>
-            <div className={style.mainContainer}>
-                <div data-aos="fade-right" data-aos-duration="1200" className={style.mainTextContainer}>
-                    <div className={style.mainTextBlock}>
-                        <span className={style.mainText}>Hi There, </span>
-                        <span className={style.mainNameText}><span
-                            className={style.mainText}>I'm </span>Ivan Osipik</span>
-                    </div>
+            <div>
+                <Particles className={style.particles} params={particlesOptions}/>
+                <div className={style.particlesContent}>
+                    <div className={style.mainContainer}>
+                        <div data-aos="fade-right" data-aos-duration="1200" className={style.mainTextContainer}>
+                            <div className={style.mainTextBlock}>
+                                <span className={style.mainText}>Hi There, </span>
+                                <span className={style.mainNameText}><span
+                                    className={style.mainText}>I'm </span>Ivan Osipik</span>
+                            </div>
 
-                    <div>
-                        <ReactTypingEffect
-                            className={style.mainPositionText}
-                            text={'Front-end developer (React).'}
-                            speed={100}
-                            eraseSpeed={50}
-                            eraseDelay={7000}
-                            typingDelay={0}
-                        />
+                            <div>
+                                <ReactTypingEffect
+                                    className={style.mainPositionText}
+                                    text={'Front-end developer (React).'}
+                                    speed={100}
+                                    eraseSpeed={50}
+                                    eraseDelay={7000}
+                                    typingDelay={0}
+                                />
+                            </div>
+                        </div>
+                        <div data-aos="fade-left" data-aos-duration="1200" className={style.mainPhoto}
+                             style={props.style}>
+                        </div>
                     </div>
                 </div>
-                    <div data-aos="fade-left" data-aos-duration="1200" className={style.mainPhoto}
-                         style={props.style}>
-                    </div>
             </div>
+            {/*<div className={style.mainContainer}>*/}
+            {/*    <div data-aos="fade-right" data-aos-duration="1200" className={style.mainTextContainer}>*/}
+            {/*        <div className={style.mainTextBlock}>*/}
+            {/*            <span className={style.mainText}>Hi There, </span>*/}
+            {/*            <span className={style.mainNameText}><span*/}
+            {/*                className={style.mainText}>I'm </span>Ivan Osipik</span>*/}
+            {/*        </div>*/}
+
+            {/*        <div>*/}
+            {/*            <ReactTypingEffect*/}
+            {/*                className={style.mainPositionText}*/}
+            {/*                text={'Front-end developer (React).'}*/}
+            {/*                speed={100}*/}
+            {/*                eraseSpeed={50}*/}
+            {/*                eraseDelay={7000}*/}
+            {/*                typingDelay={0}*/}
+            {/*            />*/}
+            {/*        </div>*/}
+            {/*    </div>*/}
+            {/*    <div data-aos="fade-left" data-aos-duration="1200" className={style.mainPhoto}*/}
+            {/*         style={props.style}>*/}
+            {/*    </div>*/}
+            {/*</div>*/}
         </section>
     )
 }
